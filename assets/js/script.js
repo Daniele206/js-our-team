2dd465e908a80b143a73c6943308ec5571947e7d
Daniele206 <danieledeieso2004@gmail.com>
2024-03-05 14:07:04 +0100

    [add] array, [add] objects to array, [add] property to objects.

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const team = [
+  {
+    name: 'Wayne Barnet',
+    assignament: 'Founder & CEO',
+    img: "./assets/img/wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: 'Angela Caroll',
+    assignament: 'Chief Editor',
+    img: "./assets/img/angela-caroll-chieff-editor.jpg",
+  },
+  {
+    name: 'Walter Gordon',
+    assignament: 'Office Manager',
+    img: "./assets/img/walter-gordon-office-manager.jpg",
+  },
+  {
+    name: 'Angela Lopez',
+    assignament: 'Social Media Manager',
+    img: "./assets/img/angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: 'Scott Estrada',
+    assignament: 'Developper',
+    img: "./assets/img/scott-estraqda-developper.jpg",
+  },
+  {
+    name: 'Barbara Ramos',
+    assignament: 'Graphic Designer',
+    img: "./assets/img/barbara-ramos-graphic-designer.jpg",
+  },
+];
